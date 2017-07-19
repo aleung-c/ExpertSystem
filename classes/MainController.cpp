643d2128eaf304c593ee04aba5ca1da790148983
aleung-c <aleung-c@e3r9p19.42.fr>
2017-07-19 13:22:56 +0200

    push multi file error handling

--- classes/MainController.cpp
+++ classes/MainController.cpp
@@ -34,20 +34,27 @@ void	MainController::Run(int argc, char **argv)
 		for (std::vector<t_ExpSysFile>::iterator it = ExpertSystemDatas->Files.begin();
 			it != ExpertSystemDatas->Files.end(); it++)
 		{
-			std::cout << std::endl << KCYN "------------------------------------------- " KRESET << std::endl;
-			std::cout << KCYN "  File: " << (*it).Path << std::endl;
-			std::cout << KCYN "------------------------------------------- " KRESET << std::endl;
-			InputController.print(*it);
-			InputController.LexParse(*it);
-			InputController.FillValues(*it);
+			try
+			{
+				std::cout << std::endl << KCYN "------------------------------------------- " KRESET << std::endl;
+				std::cout << KCYN "  File: " << (*it).Path << std::endl;
+				std::cout << KCYN "------------------------------------------- " KRESET << std::endl;
+				InputController.print(*it);
+				InputController.LexParse(*it);
+				InputController.FillValues(*it);
 
-			std::cout << KYEL "After Input: ------------" KRESET << std::endl;
-			InputController.print(*it);
-		
-			// II. Resolution handling
-			Game = GameController((*it).Query, InputController.getAllFacts(*it), ExpertSystemDatas->Verbose);
-			std::cout << Game;
-			Game.run();
+				std::cout << KYEL "After Input: ------------" KRESET << std::endl;
+				InputController.print(*it);
+
+				// II. Resolution handling
+				Game = GameController((*it).Query, InputController.getAllFacts(*it), ExpertSystemDatas->Verbose);
+				std::cout << Game;
+				Game.run();
+			}
+			catch (CustomException &e)
+			{
+				std::cout << std::endl << KRED "Error in File: " KYEL << (*it).Path << KRESET " : " << e.what() << std::endl;
+			}
 		}
 	}
 	catch (CustomException &e)
